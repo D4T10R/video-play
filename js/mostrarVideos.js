2869a618f980dd74b74126d7ec288a5ae36df5ab
Datior <leonardowederveiga@yahoo.com>
2023-03-10 23:25:41 -0300

    Exibindo elementos

---
+++ js/mostrarVideos.js
@@ -0,0 +1,33 @@
+import { conectaApi } from "./conexaoApi.js"
+
+
+const lista = document.querySelector("[data-lista]")
+
+function criarCard(titulo, descricao, url, imagem) {
+    const li = document.createElement('li')
+    li.className = 'videos__item'
+    
+    li.innerHTML = `
+            <iframe width="100%" height="72%" src="${url}"
+                title="${titulo}" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img src="${imagem}" alt="logo canal alura">
+                <h3>${titulo}</h3>
+                <p>${descricao}</p>
+            </div>
+    `
+    return li
+}
+
+
+async function listaVideos() {
+    const listaApi = await conectaApi.listaVideos();
+
+    listaApi.forEach(element => lista.appendChild(criarCard(element.titulo, element.descricao, element.url, element.imagem)));
+
+}
+
+
+listaVideos()
